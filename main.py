af75f5a9485e230f98ab089ebb268968ce899824
stopsimping <gaspard.chevalier@etu.u-paris.fr>
2022-09-29 14:30:21 +0200

    working p
    no more api call, working with person database i've created, faster, categories (bio, vegan...)

---
+++ main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from modules.filler import poll
+import sys,os
+import time
+import argparse
+
+def doArgs(argList, name):
+    parser = argparse.ArgumentParser(description=name)
+
+    parser.add_argument('-v', "--verbose", action="store_true", help="Enable verbose debugging", default=False)
+    parser.add_argument('--input', action="store", dest="inputFn", type=str, help="Input file name", required=True)
+    parser.add_argument('--output', action="store", dest="outputFn", type=str, help="Output file name", required=True)
+    parser.add_argument('--nb', action="store", dest="number", type=str, help="How many lines to generate", required=True)
+
+    return parser.parse_args(argList)
+
+def main():
+    progName = "Excel Random Datasets"
+    args = doArgs(sys.argv[1:], progName)
+
+    verbose = args.verbose
+    inputFn = args.inputFn
+    outputFn = args.outputFn
+    how_many = int(args.number)
+
+    startTime = float(time.time())
+
+    if not os.path.isfile(inputFn):
+        print ("Aliments file doesn't exist, exiting")
+        return
+    
+    if not os.path.isfile(outputFn):
+        print ("Sondage file doesn't exist, exiting")
+        return
+
+    try:
+        poll(how_many)
+    except Exception as err:
+        print(err)
+
+    print ("Job done in %0.4f seconds" % (time.time() - startTime))
+    return
+
+if __name__ == '__main__':
+    sys.argv = ["main.py","--input","Aliments.xlsx","--output","Sondage.xlsx","--nb","100"]
+    main()
+    os.system("pause")
+    
---
+++ modules/Person.py
@@ -0,0 +1,44 @@
+import random
+import pandas as pd
+
+url = "https://api.namefake.com/french-france/male/"
+sondage = pd.read_excel('Sondage.xlsx', sheet_name='Feuil2')
+MAX_ADMIN = 1000000
+
+# Person class structure
+class Person:
+    def __init__(self, nom, prenom, birth, address, postal_code, city, phone, aliments):
+        self.admin = self.getAdminId()
+        self.nom = nom
+        self.prenom = prenom
+        self.birth = birth
+        self.address = address
+        self.postal_code = postal_code
+        self.city = city
+        self.phone = phone
+        self.aliments = aliments
+        self.code_cli = self.format_codecli()
+
+    def format_codecli(self):
+        return self.prenom[0:2].upper() + self.nom[0:3] if self.nom[2] != " " else self.prenom[0:2].upper() + self.nom[0:2] + self.nom[3]
+
+
+    def getAdminId(self):
+        chosen = random.randint(0, MAX_ADMIN)
+        while chosen in sondage["Administré.e"]:
+            chosen = random.randint(0, MAX_ADMIN)
+        return chosen
+
+    def infos(self):
+        print('Nom : {}\nPrenom : {}\nDate de Naissance : {}\nAddresse : {}\nCode Postal : {}\nNuméro de Téléphone : {}\n'.format(self.nom, self.prenom, self.birth, self.address, self.postal_code, self.phone))
+        print("Aliments choisis :", self.aliments)
+        print("Code CLI : ", self.code_cli)
+        print("Admin :", self.admin)
+
+    def asDataFrame(self):
+        elements = [self.admin, self.nom, self.prenom, self.birth, self.address, self.postal_code, self.city, self.phone, self.aliments[0], self.aliments[1], self.aliments[2], self.aliments[3], self.aliments[4], self.aliments[5], self.aliments[6], self.aliments[7], self.aliments[8], self.aliments[9]]
+        columns_d = list(sondage.columns.values)
+        data_d = dict(zip(columns_d, elements))
+        df = pd.DataFrame([data_d])
+        return df
+
