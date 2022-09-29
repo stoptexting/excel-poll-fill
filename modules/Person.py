1695d3142135bd56a4bc182ee8f3a63554a60a52
stopsimping <gaspard.chevalier@etu.u-paris.fr>
2022-09-29 22:14:26 +0200

    bug founded but not fixed

--- modules/Person.py
+++ modules/Person.py
@@ -1,5 +1,6 @@
 import random
 import pandas as pd
+import time
 
 url = "https://api.namefake.com/french-france/male/"
 sondage = pd.read_excel('Sondage.xlsx', sheet_name='Feuil2')
@@ -68,6 +69,7 @@ class Person:
 
         for alim in self.aliments:
             alim_row = aliments_df.loc[aliments_df['alim_code'] == alim]
+            print("alim avant replace ", alim, " ", alim_row.values)
             alim_row = alim_row[['Sucres (g/100 g)', 'Sel chlorure de sodium (g/100 g)', 'AG saturés (g/100 g)', 'Polyols totaux (g/100 g)', 'Protéines, N x 625 (g/100 g)', 'Fibres alimentaires (g/100 g)']]
             # if regex not here, replace not working!!!!!!!!!!!!
             alim_row = alim_row.stack().str.replace("-", "0", regex=True).str.replace('[A-Za-z]', '', regex=True).str.replace(',', '.', regex=True).str.replace('<', '', regex=True).str.replace(' ', '', regex=True).str.replace('', '0', regex=True).unstack()
@@ -82,13 +84,16 @@ class Person:
             # print([sugar, salt, transfat, polyols, proteins, fibers])
             #print(alim_row.columns.values)
             alim_row.apply(lambda x: x.replace(',','.'))
+            print("before", [sugar, salt, transfat, polyols, proteins, fibers])
+            print("alim", alim, " ", alim_row.values)
             sugar += float(0.0 if 'Sucres (g/100 g)' not in alim_row.keys() else alim_row['Sucres (g/100 g)'])
             salt += float(0.0 if 'Sel chlorure de sodium (g/100 g)' not in alim_row.keys() else alim_row['Sel chlorure de sodium (g/100 g)'])
             transfat += float(0.0 if 'AG saturés (g/100 g)' not in alim_row.keys() else alim_row['AG saturés (g/100 g)'])
             polyols += float(0.0 if 'Polyols totaux (g/100 g)' not in alim_row.keys() else alim_row['Polyols totaux (g/100 g)'])
             proteins += float(0.0 if 'Protéines, N x 625 (g/100 g)' not in alim_row.keys() else alim_row['Protéines, N x 625 (g/100 g)'])
             fibers += float(0.0 if 'Fibres alimentaires (g/100 g)' not in alim_row.keys() else alim_row['Fibres alimentaires (g/100 g)'])
-            
+            print("after", [sugar, salt, transfat, polyols, proteins, fibers])
+            time.sleep(10)
         # df_all = pd.DataFrame()
         # for alim in self.aliments:
         #     # print(self.aliments)
@@ -107,6 +112,19 @@ class Person:
         #     print(df_all[col].values)
         #     nutrifactados_list.append(df_all[col].sum())
         # # print(nutrifactados_list)
+
+        # ''' bug vient du fait que en float ça met des 0 partout
+        # alim avant replace  40057   [[40057 'Coeur, dinde, cuit' nan 4 'viandes, œufs, poissons et assimilés'
+        # 401 'viandes cuites' 40108 'abats halal' '701' '167' '701' '167' '67,2'
+        # '24,9' '24,9' '0' '7,52' '0' '-' '-' '-' '-' '-' '-' '-' '0' '0' '1,21'
+        # '0' 'traces' '1,9' '1,67' '1,83' '0' '0' '0' '0,004' '0,054' '0,064'
+        # '1,12' '0,63' '1,48' '1,39' '0,049' '-' '0,009' '0,005' '359' '0,35'
+        # '21' '-' '0,83' '6,96' '-' '28' '0,12' '241' '203' '-' '140' '4,6' '16'
+        # '0' '0' '0,13' '0' '-' '-' '0,24' '1,54' '7,76' '1,58']]
+        # before [0.0, 0.0815, 110.301, 0.0, 9140.06, 0.0]
+        # alim 40057   [['000' '000.03050' '010.090' '000' '02040.090' '000']]
+        # after [0.0, 0.112, 120.391, 0.0, 11180.15, 0.0]
+        # '''
 
         print([sugar, salt, transfat, polyols, proteins, fibers])
         return [sugar, salt, transfat, polyols, proteins, fibers]
